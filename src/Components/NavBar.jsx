e77a21667dd7b4386dad7527cf2285a94e6bdb3a
Kehinde Balogun <balogunkehinde3@gmail.com>
2023-10-23 05:10:21 -0700

    deleted an unused div

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import logo from '../assets/icon.png'
+import React from "react";
+import logo from "../assets/icon.png";
 
 function NavBar() {
   return (
-    <nav className='flex justify-between bg-gray-600 text-white p-2 w-full z-10'>
-       <div className='flex gap-5 align-middle'>
-       <img src={logo} alt="github logo" width={"30px"}/>
-        <h1 className='text-xl'>My GitHub Portfolio</h1>
-        </div> 
-        <div>
-            
-        </div>
+    <nav className="flex justify-between bg-gray-600 text-white p-2 w-full z-10">
+      <div className="flex gap-5 align-middle">
+        <img src={logo} alt="github logo" width={"30px"} />
+        <h1 className="text-xl">My GitHub Portfolio</h1>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
